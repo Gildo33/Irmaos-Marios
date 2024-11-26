6621b14c02e9b2ee5f9e63ad79551441f2581aec
Gildo33 <gildofelismino38@gmail.com>
2024-11-26 19:03:40 -0300

    Meu projto irmaos mario

---
+++ script.js
@@ -0,0 +1,53 @@
+const form = document.querySelector(".Formulario")
+const mascara = document.querySelector(".Mascara-Formulario")
+const home1 = document.querySelector(".home1")
+const FaleConosco = document.querySelector(".Fale-Conosco")
+
+
+
+
+function Cliqueaqui() {
+    form.style.left = "40%"
+    
+    
+}
+
+function Tela() {
+    form.style.left ="-210px"
+
+}
+
+function home() {
+    mascara.style.visibility = "visibility "
+    form.style.left ="-240px"
+
+}
+
+function Contato() {
+    form.style.left = "40%"
+    
+     
+    
+}
+
+
+
+
+    
+
+
+
+
+/*
+
+
+
+*/ 
+
+
+
+
+
+
+
+ 
